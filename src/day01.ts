ced27a5f45650e2dd0609d824e7cb11bf912ef59
Timo Peters <timo.peters1702@gmail.com>
2022-12-07 15:25:48 +0100

    Update day01

--- src/day01.ts
+++ src/day01.ts
@@ -7,52 +7,36 @@ const INPUT_FILE = 'day01.txt';
 function solvePartOne(): void {
   const inputArr: string[] = Utility.readInputIntoStringArr(INPUT_FILE);
 
-  let max_cals = findMaxCals(inputArr);
-
-  console.log(`Part One: ${max_cals}`);
+  const result = Math.max(...getCalorieSums(inputArr));
+  console.log(`Part One: ${result}`);
 }
 
 /** find the TOP THREE elfes carying the most calories */
 function solvePartTwo(): void {
   const inputArr: string[] = Utility.readInputIntoStringArr(INPUT_FILE);
 
-  let lastSums: number[] = [];
-  let max_cals_sum = 0;
-
-  for(let i = 0; i <= 2; i++) {
-    const lastSum = findMaxCals(inputArr, lastSums);
-    lastSums.push(lastSum);
-    max_cals_sum += lastSum;
-  }
-
-  console.log(`Part Two: ${max_cals_sum}`);
+  const result = getCalorieSums(inputArr).sort((x,y) => y - x).slice(0,3).reduce((acc, curr) => acc + curr, 0);
+  console.log(`Part Two: ${result}`);
 }
 
-function findMaxCals(cals_arr: string[], exclude: number[] = []) {
+function getCalorieSums(inputArr: string[]): number[] {
+  return getCalorieGroups(inputArr).map(calorieGroup => {
+    return calorieGroup.reduce((acc, curr) => +acc + +curr, 0);
+  });
+};
 
-  let max_cals = 0;
-  let elf_cals: number[] = [];
-  cals_arr.forEach((val, idx) => {
-    if (idx === (cals_arr.length - 1)) {
-      elf_cals.push(+val);
-    }
-
-    if (val === '' || idx === (cals_arr.length - 1)) {
-      const sum = elf_cals.reduce((acc, curr) => acc + curr, 0);
-      
-      if (!exclude.includes(sum)) {
-        if (sum > max_cals) {
-          max_cals = sum;
-        }
-      }
-
-      elf_cals = [];
-    } else {
-      elf_cals.push(+val);
+function getCalorieGroups(inputArr: string[]): string[][] {
+  const splitArr: string[][] = [];
+  let startIdx = 0;
+  inputArr.forEach((elem, index) => {
+    if (elem === "") {
+      splitArr.push(inputArr.slice(startIdx, index));
+      startIdx = ++index;
+    } else if (index === (inputArr.length - 1)) {
+      splitArr.push(inputArr.slice(startIdx, inputArr.length));
     }
   });
-
-  return max_cals;
+  return splitArr;
 }
 
 solvePartOne();
